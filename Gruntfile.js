111f6a40c10f0284f9ffa7a93e4a00b4a9826eec
vdanyliv <volodymyr@1worldonline.com>
2015-10-31 18:20:25 +0200

    added grunt support

---
+++ Gruntfile.js
@@ -0,0 +1,11 @@
+module.exports = function(grunt) {
+	
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    
+  // Loading of tasks and registering tasks will be written here
+};
