ac4e20284124c3d51cde346d3a3d231add083b38
vdanyliv <volodymyr@1worldonline.com>
2015-11-03 21:26:26 +0200

    removed unused module

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,11 +37,7 @@ module.exports = function(grunt) {
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-requirejs'); 
     grunt.loadNpmTasks('grunt-contrib-connect');
     /*----------  default task  ----------*/
