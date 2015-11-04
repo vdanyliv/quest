6570a6ebdaee88a884d3fa0547ca4bcd79f59a77
vdanyliv <volodymyr@1worldonline.com>
2015-11-04 08:32:59 +0200

    added angular route | setting-up dev, prod mode

--- front/app/app.js
+++ front/app/app.js
@@ -1,6 +1,15 @@
-define(['angular'],
-	function(angular) {
-		console.error(angular);
+define(
+	[
+		'angular',
+		'ngRoute'
+	],
+
+	function(angular, ngRoute) {
+		var app = angular.module('quest', ['ngRoute']);
+
+		app.init = function() {
+			angular.bootstrap(document, ['quest']);
+		}
 
 		return app;
 	}
--- front/app/main.js
+++ front/app/main.js
@@ -1,11 +1,17 @@
 require.config({
 	paths: {
 		'angular': '../../node_modules/angular/angular.min',
-		'jquery': '../../node_modules/jquery/dist/jquery.min'
+		'jquery': '../../node_modules/jquery/dist/jquery.min',
+		'app': 'app',
+		'ngRoute': '../../node_modules/angular-route/angular-route.min'
 	},
 	shim: {
 		angular: {
 			exports: 'angular'
+
+		},
+		ngRoute: {
+			exports: 'ngRoute'
 		},
 		jquery: {
 			exports: 'jq'
