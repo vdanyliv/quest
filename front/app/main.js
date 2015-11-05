5d5c128e9cc5c58bf148d4eb228748edee3a8e2d
vdanyliv <volodymyr@1worldonline.com>
2015-11-06 01:34:40 +0200

    new logic for deps loading, without using shim

--- front/app/main.js
+++ front/app/main.js
@@ -1,24 +1,22 @@
 require.config({
 	paths: {
 		'angular': '../../node_modules/angular/angular.min',
-		'jquery': '../../node_modules/jquery/dist/jquery.min',
-		'app': 'app',
 		'ngRoute': '../../node_modules/angular-route/angular-route.min'
-	},
-	shim: {
-		angular: {
-			exports: 'angular'
-
-		},
-		ngRoute: {
-			exports: 'ngRoute'
-		},
-		jquery: {
-			exports: 'jq'
-		}
 	}
 });
 
-require(['app'], function (app) {
-	app.init();
-});
+require(
+	['angular',
+	'ngRoute'
+	], 
+	function() {
+		require(
+			[
+				'app'
+			],
+		 	function(app) {
+		 		app.init();
+		 	}
+		);
+	}
+);
