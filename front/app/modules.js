500b2d12b2eb8f0b104ff0f98a872c2320742e32
Volodymyr Danyliv <volodymyr@1worldonline.com>
2015-11-16 17:25:02 +0200

    new route for sign up

--- front/app/modules.js
+++ front/app/modules.js
@@ -2,15 +2,17 @@ define(
 	[
 		'controllers/mainctrl',
 		'controllers/aboutctrl',
-		'controllers/contactctrl'
+		'controllers/contactctrl',
+		'controllers/signupctrl'
 	],
-	function(mainCtrl, aboutCtrl, contactCtrl) {
+	function(mainCtrl, aboutCtrl, contactCtrl, signUpCtrl) {
 		var moduleName = 'quest.main';
 		
 		angular.module(moduleName, [])
 			.controller('mainCtrl', mainCtrl)
 			.controller('aboutCtrl', aboutCtrl)
-			.controller('contactCtrl', contactCtrl);
+			.controller('contactCtrl', contactCtrl)
+			.controller('signUpCtrl', signUpCtrl);
 
 	return moduleName;
 });
