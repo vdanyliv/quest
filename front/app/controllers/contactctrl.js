c8ad315b69b1fa506eecc9a48fddbdccd92d5f88
Volodymyr Danyliv <volodymyr@1worldonline.com>
2015-11-16 16:34:21 +0200

    added success notification

--- front/app/controllers/contactctrl.js
+++ front/app/controllers/contactctrl.js
@@ -3,12 +3,15 @@ define(function () {
  			var ref = new Firebase('https://glowing-heat-7159.firebaseio.com/contactForm');
  			
  			$scope.form = $firebaseArray(ref);
+ 			$scope.isDataSaved = false;
 
  			$scope.submitForm = function() {
  				$scope.form.$add({
  					name: $scope.form.name,
  					email: $scope.form.email,
  					question: $scope.form.question
+ 				}).then(function() {
+ 					$scope.isDataSaved = true;
  				});
  			}
  		}
